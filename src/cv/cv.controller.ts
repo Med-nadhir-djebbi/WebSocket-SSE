5f7a61047830387840b9cc668e467d7aad998522
Mohamed Nadhir Djebbi <mohamednadhirdjebbi@gmail.com>
2026-05-07 09:26:43 +0100

    final

--- src/cv/cv.controller.ts
+++ src/cv/cv.controller.ts
@@ -13,31 +13,31 @@ export class CvController {
 
   @Post()
   @UseGuards(AuthGuard('jwt')) 
-  create(@Body() createCvDto: CreateCvDto, @getUser() user: { id: number }) {
-    return this.cvService.create(createCvDto, user.id);
+  create(@Body() createCvDto: CreateCvDto, @getUser('id') userId: number) {
+    return this.cvService.create(createCvDto, userId);
   }
 
   @Get()
   @UseGuards(AuthGuard('jwt'))
-  findAll(@getUser() user: { id: number }) {
-    return this.cvService.findAll(user.id);
+  findAll(@getUser('id') userId: number) {
+    return this.cvService.findAll(userId);
   }
 
   @Get(':id')
   @UseGuards(AuthGuard('jwt'))
-  findOne(@Param('id') id: string, @getUser() user: { id: number }) {
-    return this.cvService.findOne(+id, user.id);
+  findOne(@Param('id') id: string, @getUser('id') userId: number) {
+    return this.cvService.findOne(+id, userId);
   }
 
   @Patch(':id')
   @UseGuards(AuthGuard('jwt')) 
-  update(@Param('id') id: string, @Body() updateCvDto: UpdateCvDto, @getUser() user: { id: number }) {
-    return this.cvService.update(+id, updateCvDto, user.id);
+  update(@Param('id') id: string, @Body() updateCvDto: UpdateCvDto, @getUser('id') userId: number) {
+    return this.cvService.update(+id, updateCvDto, userId);
   }
 
   @Delete(':id')
   @UseGuards(AuthGuard('jwt')) 
-  remove(@Param('id') id: string, @getUser() user: { id: number }) {
-    return this.cvService.remove(+id, user.id);
+  remove(@Param('id') id: string, @getUser('id') userId: number) {
+    return this.cvService.remove(+id, userId);
   }
 }
